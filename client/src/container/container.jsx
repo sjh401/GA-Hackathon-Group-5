6130d38c0c86494886a9fa051cc097c7eab59b7e
Coreywilson319 <coreywilson319@gmail.com>
2021-09-23 00:08:45 -0400

    container.jsx

--- client/src/container/container.jsx
+++ client/src/container/container.jsx
@@ -46,14 +46,52 @@ export default function Container(props) {
             setPet(fetchedPet)
         }
         fetchPet()
-    }
-    )
+    }, [])
 
     const addPet = async (newItem) => {
         const newPet = await postPet(newItem)
+        setAllPets(prevPets => {[
+            ...prevPets,
+            newItem
+        ]})
     }
 
     const addAppointment = async (newItem) => {
         const newAppointment = await postAppointment(newItem)
+        setAllAppointments(prevAppointments => {[
+            ...prevAppointments,
+            newItem
+        ]})
     }
+
+    const updatePet = async (updatedItem, pet_id) => {
+        const updatedPet = await putPet(updatedItem, pet_id)
+        setAllPets(prevPetData => prevPetData.map(pet => {
+            return pet.id === Number(pet_id) ? updatedPet : pet
+        }))
+    }
+
+    const updateAppointment = async (updatedItem, appointment_id) => {
+        const updatedAppointment = await putAppointment(updatedItem, appointment_id)
+        setAllAppointments(prevAppointmentData => prevAppointmentData.map(appointment => {
+            return appointment.id === Number(appointment_id) ? updatedAppointment : appointment
+        }))
+    }
+
+    const removePet = async (pet_id) => {
+        await deletePet(pet_id)
+        setAllPets(prevPetData => prevPetData.filter(pet => pet.id !== Number(pet_id)))
+    }
+    const removeAppointment = async (appointment_id) => {
+        await deleteAppointment(appointment_id)
+        setAllAppointments(prevAppointmentData => prevAppointmentData.filter(appointment => appointment.id !== Number(appointment_id)))
+    }
+
+    return (
+        <>
+        <Switch>
+
+        </Switch>
+        </>
+    )
 }
